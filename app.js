bcc6a1369f0f7acf684c07b59f8e4186b8e1244b
Abdul Majid <abdulmajidcse@gmail.com>
2021-11-24 18:30:56 +0600

    js class and inheritance example

--- app.js
+++ app.js
@@ -1,88 +1,24 @@
-let todoList = [
-    {
-        id: 1,
-        title: "My Todo 1",
-        note: "Welcome to my new todo 1",
-    },
-    {
-        id: 2,
-        title: "My Todo 2",
-        note: "Welcome to my new todo 2",
-    },
-    {
-        id: 3,
-        title: "My Todo 3",
-        note: "Welcome to my new todo 3",
-    }
-];
-
-function storeTodo() {
-    let todoTitle = prompt("Todo Title");
-    let todoNote = prompt("Todo Note");
-    let message = 'Todo Added!';
-    let storeTodoId = 1;
-    
-    if (todoList.length > 0) {
-        let lastTodoItem = todoList[todoList.length - 1];
-        storeTodoId = lastTodoItem.id + 1;
-    }
-
-    todoList.push({
-        id: storeTodoId,
-        title: todoTitle,
-        note: todoNote
-    });
-
-    getTodos();
-    alert(message);
-}
-
-function editTodo(todoId) {
-    let todoTitle = prompt("Todo Title");
-    let todoNote = prompt("Todo Note");
-    let message = 'Something went wrong to Edit!';
-    todoList = todoList.map((todo) => {
-        if (todo.id == todoId) {
-            todo = {
-                id: todoId,
-                title: todoTitle ? todoTitle : todo.title,
-                note: todoNote ? todoNote : todo.note
-            };
-            message = 'Todo Edited!';
-        }
-        return todo;
-    });
-    getTodos();
-    alert(message);
-}
-
-function deleteTodo(todoId) {
-    if (confirm("Are you sure?")) {
-        todoList = todoList.filter((todo) => todo.id != todoId);
-        getTodos();
-        alert("Todo Deleted!");
+class People {
+    constructor(name) {
+        this._name = name;
     }
 }
 
-function getTodos() {
-    let todoListDiv = document.querySelector("#todoList");
+class Student extends People {
+    constructor(name, address) {
+        super(name);
+        this._address = address;
+    }
 
-    if (todoList.length > 0) {
-        let todoListOutput = todoList
-            .map(
-                (todo) =>
-                    `<li class="list-group-item">
-            <h4>${todo.title}</h4>
-            <p>${todo.note}</p>
-            <button class="btn btn-sm btn-primary" onclick="editTodo(${todo.id})">Edit</button>
-            <button class="btn btn-sm btn-danger" onclick="deleteTodo(${todo.id})">Delete</button>
-        </li>`
-            )
-            .join("");
-        todoListDiv.innerHTML = todoListOutput;
-    } else {
-        todoListDiv.innerHTML = `<li class="list-group-item">No Todo Available!</li>`;
+    get name() {
+        return this._name;
+    }
+
+    get address() {
+        return this._address;
     }
 }
 
-getTodos();
+let student = new Student('Ahsan Habib', 'Rangpur');
+
+console.log(student.address);
