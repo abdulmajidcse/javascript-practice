b2d13a50131bacc852667a84048b497e9ac40fd7
Abdul Majid <abdulmajidcse@gmail.com>
2021-11-26 22:00:33 +0600

    Ajax example

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
-let w = new Worker('./show-time.js');
-w.onmessage = function(event) {
-    document.getElementById('text').innerHTML = event.data;
+let xhttp = new XMLHttpRequest();
+
+xhttp.onload = function() {
+    console.log(JSON.parse(this.response));
 };
+
+xhttp.open('get', 'https://talktoapi.abdulmajid.me/api/v1/todos');
+
+// for add new todo
+// let formData = new FormData();
+// formData.append('title', 'Hello Todo');
+// formData.append('note', 'Hello todo note details');
+// formData.append('comment', 'testing purpose comment');
+
+xhttp.send();
